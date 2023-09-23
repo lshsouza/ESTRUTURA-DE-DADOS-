ef3f0bd73f670b94165a17ec785240feee05ec33
LUÍS HENRIQUE SOUZA OLIVEIRA DOS SANTOS <luis.hsantos@a.ucb.br>
2023-09-23 10:03:47 -0300

    Adicionar atividade 5

---
+++ Lista 02/atividade5/bubble_sort.c
@@ -0,0 +1,21 @@
+#include "bubble sort.h"
+#include <string.h>
+#define MaxTamanhoPalavra 100
+
+void swap(char *PalavraA, char *PalavraB){
+    char PalavraTemp[MaxTamanhoPalavra];
+    strcpy(PalavraTemp, PalavraA);
+    strcpy(PalavraA, PalavraB);
+    strcpy(PalavraB, PalavraTemp);
+}
+
+void BubbleSort(char palavras[][MaxTamanhoPalavra], int NumElementos){
+    int i, j;
+    for (i = 0; i < NumElementos - 1; i++){
+        for (j = 0; j < NumElementos - i - 1; j++){
+            if (strcmp(palavras[j], palavras[j + 1]) > 0){
+                swap(palavras[j], palavras[j + 1]);
+            }
+        }
+    }
+}
