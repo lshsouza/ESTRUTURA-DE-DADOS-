e8bacf31b59f048d8392870434a3a50403dcda89
lshsouza <117779935+lshsouza@users.noreply.github.com>
2023-09-23 09:39:36 -0300

    Update main.c

---
+++ Lista 02/atividade4/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bubble_sort.h"
+
+
+
+int main(int argc, char *argv[]) {
+    int arraySize = argc - 1;
+    int vet[arraySize];
+
+    for (int i = 0; i < arraySize; i++) {
+        int value = atoi(argv[i + 1]);
+        vet[i] = value;
+    }
+
+    bubble_sort(vet, arraySize);
+
+    for (int i = 0; i < arraySize; i++) {
+        printf("%d ", vet[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
